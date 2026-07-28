21764d2af9d34192fdf1bbbaaed7a5d7e4c6ad50
Bhaskar Maka <bhaskar.maka@sasi.ac.in>
2026-07-28 09:30:49 +0530

    my first commit

--- myjavaproject/src/com/javaintro/GarbageCollectionDemo.java
+++ myjavaproject/src/com/javaintro/GarbageCollectionDemo.java
@@ -1,18 +1,44 @@
 package com.javaintro;
 
 public class GarbageCollectionDemo {
+	void hello() {
+		System.out.println("Hello!!");
+//		4.Object inside method
+		GarbageCollectionDemo obj=new GarbageCollectionDemo();
+	}
 	
+	@Override
+	protected void finalize() throws Throwable {
+		System.out.println("Finalize method called");
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		GarbageCollectionDemo obj1 = new GarbageCollectionDemo();
 		GarbageCollectionDemo obj2 = new GarbageCollectionDemo();
+		GarbageCollectionDemo obj3=new GarbageCollectionDemo();
 		
+//		System.out.println(obj1);
+//		System.out.println(obj2);
+		
+//		1.Nullifying object
 		obj1 = null;
-
-		obj2 = new GarbageCollectionDemo();
+//		obj2 = null;
+//		System.out.println(obj1);
+//		System.out.println(obj2);
 		
-        System.gc();                  
-		Runtime.getRuntime().gc();    
+//		2.Reassigning object
+        GarbageCollectionDemo obj4=new GarbageCollectionDemo();
+        obj2=obj4;
+        
+//      3.Anonymous object
+        new GarbageCollectionDemo().hello();
+        
+        System.gc(); 
+        System.out.println(obj1);
+		System.out.println(obj2);
+        System.out.println(obj3);
+        System.out.println(obj4);
+		                 
+		   
 
 		System.out.println("End of main method.");
 	}
---
+++ myjavaproject/src/com/javaintro/Pencils.java
@@ -0,0 +1,14 @@
+package com.javaintro;
+
+public class Pencils {
+	
+	public static void main(String[] args) {
+		int cost=7;
+		int rupees=100;
+		System.out.println("Totals Pencils: "+rupees/cost);
+		System.out.println("Remaning Balance: "+rupees%cost);
+		
+
+	}
+
+}
